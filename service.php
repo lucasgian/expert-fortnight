122a57f1882274338396e174af7ffffde2f3028f
lucasgian <gianextensao@gmail.com>
2018-04-05 11:30:51 -0400

    implementação do padrão de respostas http (post, get, put, delete)

--- service.php
+++ service.php
@@ -5,9 +5,53 @@ Class Service {
 
 	function __construct() {}
 
-	function insert($task) {
-		$fp = fopen('task.json', 'a');
-		fwrite($fp, json_encode($task->array()));
+	function post($task) {
+		$file_container = $this->open_file('task.json');
+		array_push($file_container, $task->array());
+
+		$this->write_w('task.json', $file_container);
+	}
+	function get($id) {
+		$json = $this->open_file('task.json');
+		
+		foreach($json as $register):
+		    if($id == $register->id):
+		    	echo $register->id;
+		    	return  $register;
+		    endif;
+		endforeach;
+	}
+	function put($id, $data) {
+		$json = $this->open_file('task.json');
+		$update_json = array();
+
+		foreach($json as $register):
+		    if($id == $register->id):
+		    	$register = $data;
+		    endif;
+			array_push($update_json, $register);
+		endforeach;	
+
+		$this->write_w('task.json', $update_json);
+	}
+	function delete($id) {
+		$json = $this->open_file('task.json');
+		$update_json = array();
+
+		foreach($json as $register):
+		    if($id != $register->id):
+		    	array_push($update_json, $register);
+		    endif;
+		endforeach;	
+
+		$this->write_w('task.json', $update_json);
+	}
+	function open_file($file_name) {
+		return json_decode(file_get_contents($file_name));
+	}
+	function write_w($file_name, $data) {
+		$fp = fopen($file_name, 'w');
+		fwrite($fp, json_encode($data));
 		fclose($fp);
 	}
 }
